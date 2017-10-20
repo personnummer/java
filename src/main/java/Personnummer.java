7af633e7950d652cf09fde53a52ee0457b97a9e3
Johannes <johannes@jitesoft.com>
2017-10-20 14:05:20 +0200

    verify renamed to valid to keep the interface intact.

--- src/main/java/Personnummer.java
+++ src/main/java/Personnummer.java
@@ -1,11 +1,23 @@
 public final class Personnummer {
 
 
-    public static boolean verify(String value) {
+    public static boolean valid(String value) {
         return false;
     }
 
-    public static boolean verify(long value) {
+    public static boolean valid(long value) {
         return false;
     }
+
+
+    private static int luhn(String value) {
+        return 0;
+    }
+
+    private static boolean testDate(int year, int month, int day) {
+        return false;
+    }
+
+
+
 }
--- src/test/java/PersonnummerTest.java
+++ src/test/java/PersonnummerTest.java
@@ -8,36 +8,36 @@ public class PersonnummerTest {
 
     @Test
     public void testVerifySuccess() {
-        assertTrue(Personnummer.verify("510818-9167"));
-        assertTrue(Personnummer.verify("19900101-0017"));
-        assertTrue(Personnummer.verify("19130401+2931"));
-        assertTrue(Personnummer.verify("196408233234"));
-        assertTrue(Personnummer.verify("0001010107"));
-        assertTrue(Personnummer.verify("000101-0107"));
-        assertTrue(Personnummer.verify("701063-2391"));
-        assertTrue(Personnummer.verify("640883-3231"));
+        assertTrue(Personnummer.valid("510818-9167"));
+        assertTrue(Personnummer.valid("19900101-0017"));
+        assertTrue(Personnummer.valid("19130401+2931"));
+        assertTrue(Personnummer.valid("196408233234"));
+        assertTrue(Personnummer.valid("0001010107"));
+        assertTrue(Personnummer.valid("000101-0107"));
+        assertTrue(Personnummer.valid("701063-2391"));
+        assertTrue(Personnummer.valid("640883-3231"));
 
-        assertTrue(Personnummer.verify(6403273813L));
-        assertTrue(Personnummer.verify(5108189167L));
-        assertTrue(Personnummer.verify(199001010017L));
-        assertTrue(Personnummer.verify(7010632391L));
-        assertTrue(Personnummer.verify(6408833231L));
+        assertTrue(Personnummer.valid(6403273813L));
+        assertTrue(Personnummer.valid(5108189167L));
+        assertTrue(Personnummer.valid(199001010017L));
+        assertTrue(Personnummer.valid(7010632391L));
+        assertTrue(Personnummer.valid(6408833231L));
     }
 
     @Test
     public void testVerifyFail() {
-        assertFalse(Personnummer.verify("640327-381"));
-        assertFalse(Personnummer.verify("510818-916"));
-        assertFalse(Personnummer.verify("19900101-001"));
-        assertFalse(Personnummer.verify("100101+001"));
-        assertFalse(Personnummer.verify("900161-0017"));
-        assertFalse(Personnummer.verify("640893-3231"));
+        assertFalse(Personnummer.valid("640327-381"));
+        assertFalse(Personnummer.valid("510818-916"));
+        assertFalse(Personnummer.valid("19900101-001"));
+        assertFalse(Personnummer.valid("100101+001"));
+        assertFalse(Personnummer.valid("900161-0017"));
+        assertFalse(Personnummer.valid("640893-3231"));
 
-        assertFalse(Personnummer.verify(640327381L));
-        assertFalse(Personnummer.verify(510818916L));
-        assertFalse(Personnummer.verify(19900101001L));
-        assertFalse(Personnummer.verify(100101001L));
-        assertFalse(Personnummer.verify(9001610017L));
-        assertFalse(Personnummer.verify(6408933231L));
+        assertFalse(Personnummer.valid(640327381L));
+        assertFalse(Personnummer.valid(510818916L));
+        assertFalse(Personnummer.valid(19900101001L));
+        assertFalse(Personnummer.valid(100101001L));
+        assertFalse(Personnummer.valid(9001610017L));
+        assertFalse(Personnummer.valid(6408933231L));
     }
 }
