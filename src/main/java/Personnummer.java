5daa4b0b4a78c4e9ca7eb671d03a6a63a4daf467
Johannes <johannes@jitesoft.com>
2017-10-20 16:05:51 +0200

    Implemented `valid` methods.

--- src/main/java/Personnummer.java
+++ src/main/java/Personnummer.java
@@ -1,12 +1,53 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class used to validate Swedish social security numbers.
+ *
+ * @author Johannes Tegnér
+ */
 public final class Personnummer {
+    private static final Pattern regexPattern;
 
-
-    public static boolean valid(String value) {
-        return false;
+    static {
+        regexPattern = Pattern.compile("(\\d{2}){0,1}(\\d{2})(\\d{2})(\\d{2})([-|+]{0,1})?(\\d{3})(\\d{0,1})");
     }
 
+    /**
+     * Validate a Swedish social security number.
+     *
+     * @param value Social security number to validate, as string.
+     * @return True if valid.
+     */
+    public static boolean valid(String value) {
+        Matcher matches = regexPattern.matcher(value);
+        if (!matches.find()) {
+            return false;
+        }
+
+        int year, month, day, control, number;
+        try {
+            year    = Integer.parseInt(matches.group(2).substring(2));
+            month   = Integer.parseInt(matches.group(3));
+            day     = Integer.parseInt(matches.group(4));
+            number  = Integer.parseInt(matches.group(6));
+            control = Integer.parseInt(matches.group(7));
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        boolean valid = luhn(String.format("%d%d%d%d", year, month, day, number)) == control;
+        return valid && (testDate(year, month, day) || testDate(year, month, day - 60));
+    }
+
+    /**
+     * Validate a Swedish social security number.
+     *
+     * @param value Social security number to validate, as long.
+     * @return True if valid.
+     */
     public static boolean valid(long value) {
-        return false;
+        return valid(Long.toString(value));
     }
 
 
@@ -17,7 +58,5 @@ public final class Personnummer {
     private static boolean testDate(int year, int month, int day) {
         return false;
     }
-
-
 
 }
