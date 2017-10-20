2ae2a4056e21d5425a735e294b42aebf3081018b
Johannes <johannes@jitesoft.com>
2017-10-20 12:55:35 +0200

    Initial commit. Set up gradle java-lib project.

---
+++ src/main/java/Personnummer.java
@@ -0,0 +1,10 @@
+public class Personnummer {
+
+    public boolean verify(String value) {
+        return false;
+    }
+
+    public boolean verify(Long value) {
+        return false;
+    }
+}
---
+++ src/test/java/PersonnummerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonnummerTest {
+
+    @Test
+    public void testVerifySuccess() {}
+
+    @Test
+    public void testVerifyFail() {}
+}
