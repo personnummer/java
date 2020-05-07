4fe288d73f892a82b0b5f5f3684cf4aa87d8da1f
Johannes Tegnér <johannes@jitesoft.com>
2020-05-07 09:41:13 +0200

    Updated to latest junit.

--- src/test/java/PersonnummerTest.java
+++ src/test/java/PersonnummerTest.java
@@ -1,6 +1,6 @@
-import org.junit.Test;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PersonnummerTest {
 
