d39b268d8a6e795b3547916dbed3743b498e7107
Johannes Tegnér <johannes@jitesoft.com>
2023-03-12 10:45:24 +0100

    feat(test): Implemented tests for interim numbers.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

---
+++ src/test/java/InterimnummerTest.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+import dev.personnummer.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InterimnummerTest {
+    private static Options opts = new Options(true, true);
+
+    @BeforeAll
+    public static void setup() throws IOException {
+        DataProvider.initialize();
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getValidInterimNumbers")
+    public void testValidateInterim(PersonnummerData ssn) {
+        assertTrue(Personnummer.valid(ssn.longFormat, opts));
+        assertTrue(Personnummer.valid(ssn.shortFormat, opts));
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getInvalidInterimNumbers")
+    public void testValidateInvalidInterim(PersonnummerData ssn) {
+        assertFalse(Personnummer.valid(ssn.longFormat, opts));
+        assertFalse(Personnummer.valid(ssn.shortFormat, opts));
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getValidInterimNumbers")
+    public void testFormatLongInterim(PersonnummerData ssn) throws PersonnummerException {
+        Personnummer pnr = Personnummer.parse(ssn.longFormat, opts);
+
+        assertEquals(pnr.format(false), ssn.longFormat);
+        assertEquals(pnr.format(), ssn.separatedFormat);
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getValidInterimNumbers")
+    public void testFormatShortInterim(PersonnummerData ssn) throws PersonnummerException {
+        Personnummer pnr = Personnummer.parse(ssn.shortFormat, opts);
+
+        assertEquals(pnr.format(false), ssn.longFormat);
+        assertEquals(pnr.format(), ssn.separatedFormat);
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getInvalidInterimNumbers")
+    public void testInvalidInterimThrows(PersonnummerData ssn) {
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.shortFormat, opts));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.longFormat, opts));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.separatedLong, opts));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.separatedFormat, opts));
+    }
+
+    @ParameterizedTest
+    @MethodSource("DataProvider#getValidInterimNumbers")
+    public void testInterimThrowsIfNotActive(PersonnummerData ssn) {
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.shortFormat));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.longFormat));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.separatedLong));
+        assertThrows(PersonnummerException.class, () -> Personnummer.parse(ssn.separatedFormat));
+    }
+}
